ad059567e76b3616d73892fe2384ce1732615fc0
Jeffrey Royer <jeffrey.royer@gmail.com>
2014-01-08 00:12:51 -0600

    Fixed chapter 3 exercise 2. Didn't read the instructions well enough the first time.

--- ch3/ex3-2.pl
+++ ch3/ex3-2.pl
@@ -3,6 +3,8 @@
 # Chapter 3 Example 2
 # Read a list of numbers on separate lines until end of input (Ctrl-D), then
 # prints the corresponding value (from the provided array) for each number
+# Example:
+# 1, 2, 4, 2 => fred, betty, dino, betty
 
 # Since we are using warnings here, values outside the bounds of the array
 # will cause a warning to be printed to the screen
@@ -11,14 +13,14 @@ use warnings;
 use utf8;
 
 my @names = qw{fred betty barney dino wilma pebbles bamm-bamm};
-print "Enter in some numbers (HINT: Use 0-$#names):\n";
+print "Enter in some numbers (HINT: Use 1-", scalar @names, "):\n";
 
 # Read all input and chomp it. Ctrl-D will escape this line.
 chomp(my @indices = <STDIN>);
 
 print "Your inputs from the array:\n";
 foreach $index (@indices) {
-	print $names[$index] . "\n";
+	print $names[$index-1] . "\n";
 }
 
 
